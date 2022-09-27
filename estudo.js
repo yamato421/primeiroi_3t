1c887ccb9b50263ec34586faf0d487fcee478248
yamato421 <106193990+yamato421@users.noreply.github.com>
2022-09-27 17:46:24 -0300

    27-09

--- estudo.js
+++ estudo.js
@@ -1,1 +1,7 @@
 
+      // alert("Oi! Eu sou o Alert.")
+       //document.write("Estou na Pagina.");
+    //console.log("eu estou no console.")
+    function alo(){
+        alert("clicou no botão né mané!");
+    }
