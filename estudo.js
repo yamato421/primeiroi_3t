ca960143e3a789d72410dc40b8a0e40307840d37
yamato421 <106193990+yamato421@users.noreply.github.com>
2022-10-04 17:22:56 -0300

    Add files via upload

--- estudo.js
+++ estudo.js
@@ -1,26 +1,19 @@
+// alert("Oi! Eu sou o Alert");
+// document.write("Estou na página.");
+// console.log("Eu estou no console.");
+let tabuada = 5;
 
-    
-      // alert("Oi! Eu sou o Alert.")
-       //document.write("Estou na Pagina.");
-    //console.log("eu estou no console.")
-    let tabuada = 2
-    function alo(){
-        document.write("<h1> Tabuada do  + tabuada + "<h1>")"
-        document.write(tabuada + " x 1 ="+ (tabuada * 1)+"<br>");
-        document.write(tabuada + " x 2 ="+ (tabuada * 2)+"<br>");
-        document.write(tabuada + " x 3 ="+ (tabuada * 3)+"<br>");
-        document.write(tabuada + " x 4 ="+ (tabuada * 4)+"<br>");
-        document.write(tabuada + " x 5 ="+ (tabuada * 5)+"<br>");
-        document.write(tabuada + " x 6 ="+ (tabuada * 6)+"<br>");
-        document.write(tabuada + " x 7 ="+ (tabuada * 7)+"<br>");
-        document.write(tabuada + " x 8 ="+ (tabuada * 8)+"<br>");
-        document.write(tabuada + " x 9 ="+ (tabuada * 9)+"<br>");
-        document.write(tabuada + " x 10 ="+ (tabuada * 10)+"<br>");
-    
-    
-    
-    
-    }
-    
-    
+function alo(){
+    document.write("<h1>Tabuada do " + tabuada + "</h1>")
+    document.write( tabuada + " x 1 = " + (tabuada * 1 ) + "<br>");
+    document.write( tabuada + " x 2 = " + (tabuada * 2 ) + "<br>");
+    document.write( tabuada + " x 3 = " + (tabuada * 3 ) + "<br>");
+    document.write( tabuada + " x 4 = " + (tabuada * 4 ) + "<br>");
+    document.write( tabuada + " x 5 = " + (tabuada * 5 ) + "<br>");
+    document.write( tabuada + " x 6 = " + (tabuada * 6 ) + "<br>");
+    document.write( tabuada + " x 7 = " + (tabuada * 7 ) + "<br>");
+    document.write( tabuada + " x 8 = " + (tabuada * 8 ) + "<br>");
+    document.write( tabuada + " x 9 = " + (tabuada * 9 ) + "<br>");
+    document.write( tabuada + " x 10 = " + (tabuada * 10 ));
 
+}
